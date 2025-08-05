5c06df30816df9e4ef242b46ac84eca87de91704
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-08-05 23:30:18 +0530

    docker issue

---
+++ core/settings.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(file).resolve().parent.parent
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "your-secret-key")
+
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(",")
+
+INSTALLED_APPS = [
+"django.contrib.admin",
+"django.contrib.auth",
+"django.contrib.contenttypes",
+"django.contrib.sessions",
+"django.contrib.messages",
+"django.contrib.staticfiles",
+"rest_framework",
+"core",
+]
+
+MIDDLEWARE = [
+"django.middleware.security.SecurityMiddleware",
+"django.contrib.sessions.middleware.SessionMiddleware",
+"django.middleware.common.CommonMiddleware",
+"django.middleware.csrf.CsrfViewMiddleware",
+"django.contrib.auth.middleware.AuthenticationMiddleware",
+"django.contrib.messages.middleware.MessageMiddleware",
+"django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "core.urls"
+
+TEMPLATES = [
+{
+"BACKEND": "django.template.backends.django.DjangoTemplates",
+"DIRS": [BASE_DIR / "templates"],
+"APP_DIRS": True,
+"OPTIONS": {
+"context_processors": [
+"django.template.context_processors.debug",
+"django.template.context_processors.request",
+"django.contrib.auth.context_processors.auth",
+"django.contrib.messages.context_processors.messages",
+],
+},
+},
+]
+
+WSGI_APPLICATION = "core.wsgi.application"
+
+DATABASES = {
+"default": {
+"ENGINE": "django.db.backends.postgresql",
+"NAME": os.environ.get("POSTGRES_DB", "creditdb"),
+"USER": os.environ.get("POSTGRES_USER", "user"),
+"PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+"HOST": os.environ.get("POSTGRES_HOST", "db"),
+"PORT": os.environ.get("POSTGRES_PORT", "5432"),
+}
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+{"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+{"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+{"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+{"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+"DEFAULT_PERMISSION_CLASSES": [
+"rest_framework.permissions.AllowAny",
+]
+}
+
+
